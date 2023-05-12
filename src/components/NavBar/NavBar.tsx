f9258b3a268c4bd84b2c06e6762aa87fa9364e95
Arnau Fugarolas Barbena <arnau.fugarolasbarbena@travelport.com>
2023-05-12 23:40:09 +0200

    Finish NavBar and Footer V1

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import { Logo, NavbarContainer, NavItem } from './styles'
+import React, { PropsWithChildren } from 'react'
+import {
+  Footer,
+  FooterText,
+  Logo,
+  MainContent,
+  Navbar,
+  NavItem,
+  NavLink,
+  NavLinkDisabled,
+  NavList
+} from './styles'
 
-const NavBar = () => {
+const NavBar = (props: PropsWithChildren) => {
   return (
     <>
-      <NavbarContainer>
-        <Logo src="/public/images/logo.svg" alt="logo" />
-        <NavItem>Home</NavItem>
-        <NavItem>Quest</NavItem>
-        <NavItem>Map</NavItem>
-      </NavbarContainer>
+      <Navbar>
+        <Logo src="/public/images/logo.svg" alt="Logo" />
+        <NavList>
+          <NavItem>
+            <NavLink href="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/trivial">Trivial</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/igoto">I go to... </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLinkDisabled href="/assistance">Assistance</NavLinkDisabled>
+          </NavItem>
+          <NavItem>
+            <NavLinkDisabled href="/chat">Chat</NavLinkDisabled>
+          </NavItem>
+        </NavList>
+      </Navbar>
+
+      <MainContent>{props.children}</MainContent>
+
+      <Footer>
+        <FooterText>&copy; 2023 Vueling Entertainment</FooterText>
+      </Footer>
     </>
   )
 }
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -1,30 +1,78 @@
 import styled from '@emotion/styled'
 
-export const NavbarContainer = styled.nav`
-  background-color: var(--clr-charcoal);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: sticky;
-  top: 0;
-  right: 0;
-  left: 0;
-  z-index: 1;
-  height: 5rem;
-  padding: 0 2rem;
+export const StyledLink = styled.a`
+  text-decoration: none;
+  color: inherit;
 `
 
-export const NavItem = styled.a`
-  color: var(--clr-off-white);
-  text-decoration: none;
-
-  &:hover {
-    color: #ffcc00;
-    cursor: pointer;
-  }
+export const Navbar = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+  background-color: var(--clr-charcoal);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `
 
 export const Logo = styled.img`
-  height: 3rem;
-  width: auto;
+  height: 40px;
+`
+
+export const NavList = styled.ul`
+  display: flex;
+  list-style: none;
+`
+
+export const NavItem = styled.li`
+  margin-left: 1rem;
+`
+
+export const NavLink = styled.a`
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  border-radius: 2rem;
+  transition: background-color 0.3s ease;
+  text-decoration: none;
+  color: var(--clr-off-white);
+  font-family: var(--ff-primary);
+
+  &:hover {
+    background-color: var(--clr-light-gray);
+    color: var(--clr-yellow);
+  }
+`
+export const NavLinkDisabled = styled(NavLink)`
+  opacity: 0.5;
+  cursor: not-allowed;
+  pointer-events: none;
+`
+
+export const MainContent = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 80vh;
+  padding: 2rem;
+`
+
+export const ContentImage = styled.img`
+  max-width: 100%;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`
+
+export const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  background-color: var(--clr-charcoal);
+  box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.1);
+  color: var(--clr-off-white);
+  position: sticky;
+  bottom: 0;
+`
+
+export const FooterText = styled.p`
+  font-size: 1rem;
+  font-family: var(--ff-primary);
 `
